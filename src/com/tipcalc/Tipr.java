546cfa93614b1d6320c1a58d425f1ba8218b69e2
Daniel Silva <msdaniel1995@gmail.com>
2012-08-02 23:56:25 -0500

    Implemented Listener and text view adjusted but Slider

--- src/com/tipcalc/Tipr.java
+++ src/com/tipcalc/Tipr.java
@@ -2,16 +2,25 @@ package com.tipcalc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class Tipr extends Activity implements OnSeekBarChangeListener {
+	
+	final static String TAG = "Tipr App";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipr);
+ 
+        // Must be a Listener to handle an Event
+        SeekBar tipPercentSB = (SeekBar) findViewById(R.id.tipPercent);
+        tipPercentSB.setOnSeekBarChangeListener(this);
+  
     }
 
     @Override
@@ -21,7 +30,9 @@ public class Tipr extends Activity implements OnSeekBarChangeListener {
     }
 
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-    	// TODO Auto-generated method stub
+    	TextView tipPercent = (TextView) findViewById(R.id.labelPercent);
+    	tipPercent.setText(progress + " %");
+    	Log.d(TAG, "Progress change called - from THIS listener");
     }
     
 
